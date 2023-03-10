c0d413ea912377c8b939bd39aafc22722fc91355
Dennohkamash <dennohkamash134@gmail.com>
2023-03-11 00:15:45 +0300

    python practise

---
+++ practise.py
@@ -0,0 +1,34 @@
+# name = input("Enter name:")
+# age = int(input("Enter age:"))
+# location = input("Enter location:")
+# print("hello there my name is",name,"am",age,"of age and i stay at",location)
+
+
+# input_string = input('Enter elements of a list separated by space ')
+# user_list = input_string.split()
+# for i in range(len(user_list)):
+#     user_list[i] = int(user_list[i])
+# print("Sum = ", sum(user_list))
+
+# x = (input('some text : '))
+# x = {a for a in x.split(",")}
+# print(x)
+
+
+# number = input("Enter 1st set")#takes input from users
+# number2 = input("Enter 2nd set")
+
+# number = {a for a in number.split()} #converts the input into a set and sets the slipt function
+# number2 = {b for b in number2.split()}
+# print(number)
+# print(number2) 
+
+# print(number.intersection(number2))
+
+# words=["table","food","bus","airplane","school","queen"]
+# output=[word for word in words if len(word)%2==1]
+# print(output)
+
+#words=["table","food","bus","airplane","school","queen"]
+# for word in words: 
+# print(len(word[0:]))#print length number of each element
