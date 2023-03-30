7b9adefa620d40197c1de7c20d093dcd486e5fd3
Dennohkamash <dennohkamash134@gmail.com>
2023-03-30 23:17:18 +0300

    oop

--- practise.py
+++ practise.py
@@ -38,12 +38,91 @@
 # argument and converts it to integers and sums the list. For example 
 # [‘1’, ‘3’, ‘5’] should be converted to [1, 3, 5] and summed to 9.
 
-def convert_add():
-    numbers = ["1","2","3","4","5"]
-    output =[]
-    for i in numbers:
-        output.append(int(i))
-    print(numbers)
-    print(output)
-    print(sum(output))
-convert_add()
+# def convert_add():
+#     numbers = ["1","2","3","4","5"]
+#     output =[]
+#     for i in numbers:
+#         output.append(int(i))
+#     print(numbers)
+#     print(output)
+#     print(sum(output))
+# convert_add()
+
+#creating a function that calculates the area of a triangle.
+# def area_of_triangle():
+#     base = 12
+#     height = 7
+#     area = 0.5*base*height
+#     print(area)
+# area_of_triangle()
+
+
+#methods
+# class person:
+#     def person_details(self):
+#         name = "Loveness"
+#         Nationality = "SA"
+#         print (f"Hello there my name is{name} and am from{Nationality}")
+# P1=person()#create an object
+# (P1.person_details())
+
+#  Write a Python class BankAccount with attributes like account_number, 
+#   balance, date_of_opening and customer_name, 
+#  and methods like deposit, withdraw, and check_balance,customer_details.
+#one should be able to check balance,withdraw and  deposit.
+
+class BankAccount:
+    #initializing a constructor with attributes
+    def __init__(self,account_number,balance,date_of_opening,customer_name):
+        self.account_number = input("Enter acc no")
+        self.balance = balance
+        self.date_of_opening = input("Enter opening date")
+        self.customer_name = input("Enter name")
+    #create method deposit
+    def deposit(self,amount):
+        # self.balance = self.balance + amount
+        self.balance += amount
+        print(f"{amount} has been deposited on your account")
+    #method withdraw
+    def withdraw(self,amount):
+        if amount > self.balance:
+            print(f"Insufficient funds your current balance is{self.balance}")
+        else:
+            # self.balance = self.balance - amount
+            self.balance -= amount
+            print(f"withdrawal succeful you have withdrawn {amount} you current balance is {self.balance}")
+    #method checkbalance
+    def checkbalance(self):
+        print(f"current balance is {self.balance}")
+    #method customer details
+    def customer_details(self):
+        print(f"Name:{self.customer_name}")
+        print(f"Accountnumber:{self.account_number}")
+        print(f"Date of Opening:{self.date_of_opening}")
+        print(f"Balance:{self.balance}")
+#inputing customer details
+customer1=BankAccount()
+customer2=BankAccount()
+customer3=BankAccount()
+customer4=BankAccount()
+#calling the methods customer details
+customer1.customer_details()
+# customer2.customer_details()
+#checkbalance
+# customer3.checkbalance()
+#withdrawing
+# customer4.withdraw(30000)
+# customer4.withdraw(20000)
+
+
+
+
+
+
+
+
+
+
+
+ 
+    
